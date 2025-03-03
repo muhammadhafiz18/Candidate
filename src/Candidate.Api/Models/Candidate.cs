f2f078b42d3cc80d58ebd17562ffeb830d9f9fcd
muhammadhafiz <khodjayev.muhammad@gmail.com>
2025-03-03 12:07:33 +0500

    model of candidate added

---
+++ src/Candidate.Api/Models/Candidate.cs
@@ -0,0 +1,13 @@
+namespace Candidate.Api.Models;
+
+public class Candidate
+{
+    public required string FirstName { get; set; }
+    public required string LastName  { get; set; }
+    public required string Email { get; set; }
+    public string? PhoneNumber  { get; set; }
+    public string? PreferredCallTime { get; set; }
+    public string? LinkedInUrl { get; set; }
+    public string? GitHubUrl { get; set; }
+    public required string Comment { get; set; }
+}
